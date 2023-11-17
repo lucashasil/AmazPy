a8455d546feeb5737324ce8eb00dfb720f6256e7
Lucas <lucashasil@gmail.com>
2023-11-16 18:13:45 -0800

    Add headless mode for running the script without GUI

--- amazpy/main.py
+++ amazpy/main.py
@@ -1,5 +1,23 @@
+import argparse
 from amazpy.app import App
+from amazpy.headless import Headless
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="A Python based Amazon price tracker and scraper."
+    )
+
+    parser.add_argument(
+        "--headless",
+        action=argparse.BooleanOptionalAction,
+        required=False,
+        help="Whether to run the app in headless mode or not.",
+    )
+
+    args = parser.parse_args()
+
+    if args.headless:
+        headless = Headless()
+    else:
+        app = App()
+        app.mainloop()
